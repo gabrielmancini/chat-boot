604ba615064fd008a9f66526ea205f6c6a1b5548
Gabriel Mancini <gabriel.mancini@gmail.com>
2018-02-02 17:38:42 -0200

    chat ok

--- index.js
+++ index.js
@@ -1,120 +1,51 @@
 'use strict'
+const dbg = require('util').debuglog('chat')
 const BootBot = require('bootbot')
 const guide = require('./chat.json')
-console.log(process.env.FACEBOOK)
 const bot = new BootBot({
-  'accessToken': 'EAAVZAVn3QQ0EBAGheJjUUrkSSEjeTpR00HxLgCjKPpZBzWJj0kyHf4LBMYaimP9prT0PFNc9gw6kqLT4m5z2KSVeRUSBBAWZBKy74IPPynI6cKuRQE1JEsAgTWKZB1kkGosq3lkj3s5RTOUF2NEd8uyS1cT12Osf41VLLKizUgZDZD',
-  'verifyToken': 'batman@00',
-  'appSecret': 'c2052e751440e90db0781d4e817d3c77'
+  'accessToken': process.env.FACEBOOK_ACCESS_TOKEN,
+  'verifyToken': process.env.FACEBOOK_VERIFY_TOKEN,
+  'appSecret': process.env.FACEBOOK_APP_SECRET
 })
+const session = {}
 
 function walk (guide, cb, level) {
   level = level || 0
   level++
-  var subject = Object
-    .keys(guide)
-    .map(e => ({k: e, v: guide[e]}))
-    .filter(e => Array.isArray(e.v))
-    .reduce((a, c) => { return a.v })
-  if (subject && subject.v) {
-    subject.v
-      .forEach(e => {
-        if (e && Array.isArray(e.subject)) {
-          walk(e, cb, level)
-        } else {
-          cb(e, level)
-        }
-      })
+  if (guide.subject) {
+    guide.subject
+      .forEach(e => walk(e, cb, level))
   }
+  cb(guide, level)
 }
 
-walk(guide, (element) => { console.log(element) })
+dbg('guide', guide)
+
+// walk(guide, (element) => { dbg(element) })
 walk(guide, (element, level) => {
   if (element.hear) {
+    dbg('hear', element.hear)
     bot.hear(element.hear.split('|'), (payload, chat) => {
-      chat.say(element.say, { typing: true })
+      dbg('on-hear: %o', payload)
+      session[payload.sender.id] = session[payload.sender.id] || []
+      session[payload.sender.id].push('text', payload.message.text)
+      var bootbotComponent = JSON.parse(JSON.stringify(element.bootbot))
+      bootbotComponent.text = element.say
+      chat.say(bootbotComponent, { typing: true }).then()
     })
   }
 })
-console.log(guide)
-// clone
-//   .forEach((element) => {
-//     bot.hear(element.user.split('|'), (payload, chat) => {
-//       chat.say(element.bot, { typing: true })
-//     })
-//   })
-// walk(guide.root, (element, level) => {
-//   if (level === 1 && !Array.isArray(element)) {
-//     bot.hear(element.user.split('|'), (payload, chat) => {
-//       chat.say(element.bot, { typing: true })
-//     })
-//   }
-// })
-// walk(guide.root, discovery())
-// walk(guide.root, (element) => { console.log(element) })
-
-bot.hear(['hello', 'hi', /hey( there)?/i], (payload, chat) => {
-  // Send a text message followed by another text message that contains a typing indicator
-  chat.say('Hello, human friend!').then(() => {
-    chat.say('How are you today?', { typing: true })
-  })
-})
-
-bot.hear(['food', 'hungry'], (payload, chat) => {
-  // Send a text message with quick replies
-  chat.say({
-    text: 'What do you want to eat today?',
-    quickReplies: ['Mexican', 'Italian', 'American', 'Argentine']
-  })
-})
-
-bot.hear(['help'], (payload, chat) => {
-  // Send a text message with buttons
-  chat.say({
-    text: 'What do you need help with?',
-    buttons: [
-      { type: 'postback', title: 'Settings', payload: 'HELP_SETTINGS' },
-      { type: 'postback', title: 'FAQ', payload: 'HELP_FAQ' },
-      { type: 'postback', title: 'Talk to a human', payload: 'HELP_HUMAN' }
-    ]
-  })
-})
 
 bot.hear('image', (payload, chat) => {
-  // Send an attachment
+// Send an attachment
   chat.say({
     attachment: 'image',
     url: 'http://example.com/image.png'
   })
 })
-
-const askName = (convo) => {
-  convo.ask(`What's your name?`, (payload, convo) => {
-    const text = payload.message.text
-    convo.set('name', text)
-    convo.say(`Oh, your name is ${text}`).then(() => askFavoriteFood(convo))
-  })
-}
-
-const askFavoriteFood = (convo) => {
-  convo.ask(`What's your favorite food?`, (payload, convo) => {
-    const text = payload.message.text
-    convo.set('food', text)
-    convo.say(`Got it, your favorite food is ${text}`).then(() => sendSummary(convo))
-  })
-}
-
-const sendSummary = (convo) => {
-  convo.say(`Ok, here's what  you told me about you:
-      - Name: ${convo.get('name')}
-      - Favorite Food: ${convo.get('food')}`)
-  convo.end()
-}
-
-bot.hear('ask me something', (payload, chat) => {
-  chat.conversation((convo) => {
-    askName(convo)
-  })
-})
-
 bot.start()
+setInterval(function () {
+  process.stdout.clearLine()  // clear current text
+  process.stdout.cursorTo(0)  // move cursor to beginning of line
+  process.stdout.write('Sessions: ' + Object.keys(session))  // write text
+}, 300)
